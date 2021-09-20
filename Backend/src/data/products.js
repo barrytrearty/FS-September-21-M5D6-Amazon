c16e5760e98d6284f287c569ddad84e03edde7ee
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-20 10:59:19 +0200

    products crud

--- Backend/src/data/products.js
+++
@@ -1,6 +0,0 @@
-// http://localhost:3001/products 
-import express from "express"
-
-const productsRouter = express.Router()
-
-export default productsRouter
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,17 +1,22 @@
-import express from "express"
-import productsRouter from "./data/products.js"
-const server = express()
+import express from "express";
+import listEndpoints from "express-list-endpoints"
+import cors from "cors"
+import productsRouter from "./servicies/products.js";
 
-const port = 3001
-
+const server = express();
+const port = 3001;
 
 const loggerMiddleware = (req, res, next) => {
-    console.log(`Request method ${req.method} -- Request URL ${req.url}`)
-    next()
-  }
-  server.use(loggerMiddleware)
-  server.use(productsRouter)
+  console.log(`Request method ${req.method} -- Request URL ${req.url}`);
+  next();
+};
+
+server.use(loggerMiddleware);
+server.use(cors())
+server.use(express.json())
+
+
+server.use("/products", productsRouter);
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-  })
-  
+  console.log(`Server running on port ${port}`);
+});
