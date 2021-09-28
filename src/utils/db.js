706395a10cc688648d978813b94e5fe7dc2d067f
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-28 19:30:09 +0100

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,13 +4,19 @@ const { Pool } = pg;
 
 const { DEPLOYED_DATABASE_URL: connectionString } = process.env;
 
-const sslConfig = isProduction
-  ? {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }
-  : {};
+// const sslConfig = isProduction
+//   ? {
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     }
+//   : {};
+
+const sslConfig = {
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 const pool = new Pool({
   connectionString,
