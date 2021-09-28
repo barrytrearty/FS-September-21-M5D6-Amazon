5e64dec5dc744aaafdf215dbac5603b53aba28de
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-28 19:40:07 +0100

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,21 +2,20 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const { DEPLOYED_DATABASE_URL: connectionString } = process.env;
+const { DEPLOYED_DATABASE_URL, DEV_DATABASE_URL, NODE_ENV } = process.env;
+const isProduction = NODE_ENV === "production";
 
-// const sslConfig = isProduction
-//   ? {
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     }
-//   : {};
+const connectionString = isProduction
+  ? DEPLOYED_DATABASE_URL
+  : DEV_DATABASE_URL;
 
-const sslConfig = {
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
+const sslConfig = isProduction
+  ? {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {};
 
 const pool = new Pool({
   connectionString,
