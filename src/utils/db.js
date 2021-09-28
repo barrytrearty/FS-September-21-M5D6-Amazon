797b88fa3a1c311d112bed6d18ce14efbd112c53
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-28 19:45:05 +0100

    fixed typo

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,12 +2,10 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const { DEPLOYED_DATABASE_URL, DEV_DATABASE_URL, NODE_ENV } = process.env;
+const { DATABASE_URL, DEV_DATABASE_URL, NODE_ENV } = process.env;
 const isProduction = NODE_ENV === "production";
 
-const connectionString = isProduction
-  ? DEPLOYED_DATABASE_URL
-  : DEV_DATABASE_URL;
+const connectionString = isProduction ? DATABASE_URL : DEV_DATABASE_URL;
 
 const sslConfig = isProduction
   ? {
