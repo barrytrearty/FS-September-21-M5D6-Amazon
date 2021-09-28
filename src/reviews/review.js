464cffd9895ec16d4e98f3033a8a1597a32537bb
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-28 15:52:44 +0100

    join working

--- src/reviews/review.js
+++ src/reviews/review.js
@@ -22,7 +22,16 @@ reviewsRouter.get("/", async (req, res, next) => {
 
 reviewsRouter.get("/:id", async (req, res, next) => {
   try {
-    const query = `SELECT * FROM reviews WHERE id = ${req.params.id}`;
+    const query = `SELECT 
+    review.id,
+    review.comment,
+    review.rate,
+    review.created_at,
+    product.name AS product_name,
+    product.brand,
+    product.category
+FROM reviews as review
+INNER JOIN products AS product ON review.product_id = product.id WHERE review.id = ${req.params.id}`;
     const result = await pool.query(query);
     if (result.rows.length > 0) {
       console.log(result.rows[0]);
@@ -33,6 +42,7 @@ reviewsRouter.get("/:id", async (req, res, next) => {
         .send({ message: `Product- ${req.params.id} is not found` });
     }
   } catch (error) {
+    console.log(error);
     next(error);
   }
 });
